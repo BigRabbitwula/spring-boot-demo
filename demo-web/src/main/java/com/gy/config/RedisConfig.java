b0271b6c7cdc0950c7ca3302a2bd2df1cde3e19b
guyu <915100322@qq.com>
2019-05-21 23:11:26 +0800

    整合通过jedis整合redis，以及一个小的demo

---
+++ demo-web/src/main/java/com/gy/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.gy.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+@PropertySource("classpath:redis.properties")
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.timeout}")
+    private int timeOut;
+
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxidle;
+
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private Long maxWaitMillis;
+
+    @Value("${spring.redis.block-when-exhausted}")
+    private boolean  blockWhenExhausted;
+
+    @Bean
+    public JedisPool redisPoolFactory(){
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxIdle(maxidle);
+        config.setMaxWaitMillis(maxWaitMillis);
+        config.setBlockWhenExhausted(blockWhenExhausted);
+        config.setJmxEnabled(true);
+        JedisPool jedisPool = new JedisPool(config,host,port,timeOut,password);
+        return jedisPool;
+    }
+}
--- demo-web/src/main/java/com/gy/controller/UserController.java
+++ demo-web/src/main/java/com/gy/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.gy.controller;
 
 import com.gy.api.Test;
+import com.gy.config.RedisConfig;
 import com.gy.vo.UserVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.util.HashMap;
 
@@ -22,18 +27,31 @@ public class UserController {
     @Value("${name}")
     private String name;
 
+    @Autowired
+    private JedisPool jedisPool;
+
     /**
      * 获取用户信息接口
      * @Author guyu
      * @Date 2019/1/11 18:10
      */
     @RequestMapping("/getUserInfo.do")
+    @ResponseBody
     public Object getUserInfo(){
         HashMap result = new HashMap(3);
         UserVO vo = new UserVO();
         Test test = new Test();
         result.put("code",0);
         result.put("data",test);
+        try{
+            Jedis jedis = jedisPool.getResource();
+//            jedis.set("guyu","hello word");
+            String aa = jedis.get("guyu");
+            System.out.println(aa);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         return result;
     }
 }
